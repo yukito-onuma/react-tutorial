61f2a213d63ebc73db656f2faca8be43963b96cc
jabo <aa>
2024-06-09 23:39:41 +0900

    day4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { List } from "./list";
 import { useState } from 'react';
+import { Form } from './form';
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示')
+  const [tab, setTab] = useState('list');
 
   const changeDescription = () => {
     setDescription('クリック後の表示です。')
@@ -10,8 +12,17 @@ function App() {
 
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('tab')}>フォーム</li>
+        </ul>
+      <hr />
+      </header>
       { description }
-      <List title="使用する言語一覧" />
+      {
+        tab === 'list' ? <List title="使用する言語一覧" /> : <Form />
+      }
       <button onClick={ changeDescription }>ボタン</button>
     </div>
   );
--- src/list.js
+++ src/list.js
@@ -1,10 +1,22 @@
-import React from 'react'
+const LANGUAGE = [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "Ruby",
+    "Java",
+    "Kotlin",
+    "Swift"
+]
 
 export const List = ({ title }) => {
     return (
         <div>
             <h4>{ title }</h4>
-            <div>リストです。</div>
+            {
+                LANGUAGE.map((lang, index) => {
+                    return <div key={index}>{ lang }</div>
+                })
+            }
         </div>
     )
 }
